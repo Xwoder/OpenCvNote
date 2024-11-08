fe7449c86d058e240bcec1606742d84fd97e18d3
Xwoder <Xwoder@qq.com>
2024-11-08 15:08:38 +0800

    feat: 添加 OpenCV 读取、显示和写入图像的示例代码
    - 新增 opencv_note_0001.py 文件
    - 实现了读取图像文件、显示图像、等待按键、写入图像至文件等基本功能
    - 使用了 OpenCV 的 namedWindow、imshow、waitKey 和 imwrite 方法

---
+++ opencv_note_0001.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+imageInputPath = 'images/Lena_gray.bmp'
+imageOutputPath = 'images/Lena_gray_copy.bmp'
+
+# 读取图像文件
+image: np.ndarray = cv2.imread(imageInputPath, flags=cv2.IMREAD_GRAYSCALE)
+if image is None:
+    raise RuntimeError(f"Failed to load image.")
+
+print(f"{image.shape = }")
+print(f"{image = }")
+
+# 窗口名称
+windowName: str = "Image"
+
+# 创建指定名称的窗口，可选操作
+cv2.namedWindow(windowName, cv2.WINDOW_NORMAL)
+
+# 在指定的窗口名称中显示图像
+# 如果没有通过 cv2.namedWindow() 创建指定名称窗口，则该方法会根据给定的名称创建一个默认的窗口
+cv2.imshow(windowName, image)
+
+# waitKey方法，用于接收键盘按键，返回按键的 ASCII 码
+# 如果 delay 为 0，则表示该方法会一直等待，直到接收到按键，否则该方法会等待指定的时间，超时则返回 -1
+keyCode: int = cv2.waitKey(delay=5000)
+print(f"{keyCode = }")
+
+# 写入图像至文件
+cv2.imwrite(imageOutputPath, image)
+
+# destroyAllWindows方法，其接受一个可选的窗口名称，用于销毁指定窗口，否则销毁全部窗口
+cv2.destroyAllWindows()
